7ea16a861c6c3a9c1bc13bd4ecd22f5dc076ffc6
luc_r21 <82374322+lucr21@users.noreply.github.com>
2023-01-11 09:35:27 +0100

    Add files via upload

---
+++ SAE105_1/article.php
@@ -0,0 +1,43 @@
+<?php
+session_start();                       // WARNING : NE PAS EFFACER CETTE LIGNE
+include('helpers/magasinHelper.php');  // WARNING : NE PAS EFFACER CETTE LIGNE
+
+
+//je crée mon tableau des produits (il sera utile...)
+
+//je crée mon tableau de commentaires
+
+//J'ajoute mes commentaires dans le tableau des produits
+
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>The Computer Shop</title>
+    <link rel="stylesheet" href="assets/site/style.css">
+    <link rel="stylesheet" href="assets/site/normalize.css">
+</head>
+
+<body>
+    <header>
+      <div class="header1">
+          <img src="assets/site/logo-white.png" height="109.75" width="250">
+          <a class="takecare"> We take care of your feet</a>
+      </div>
+      <div class="header2">
+          <a href="#">Acceuil</a>
+          <a href="#">Le shop</a>
+          <a href="#">Nos magasins</a>
+          <a href="#">Nous contacter </a>
+      </div>
+    </header>
+
+
+
+<?php
+// a votre imagination
+?>
+</body>
+</html>
